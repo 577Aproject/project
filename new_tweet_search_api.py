89846c873b7eb3d0f7ea0cd7e1d03f3481c75f38
leizhang323 <42682317+leizhang323@users.noreply.github.com>
2019-11-20 14:27:00 -0800

    Add query as input to search tweets, up to 180 requests within 15mins

--- new_tweet_search_api.py
+++ new_tweet_search_api.py
@@ -15,8 +15,10 @@ def get_7days_tweets(max_id=0):
     #    30days url
     #     url = 'https://api.twitter.com/1.1/tweets/search/30day/tweets.json'
     url = 'https://api.twitter.com/1.1/search/tweets.json'
-    query_data = [('q', '2020Election'), ('count', '100')]
-    if max_id != 0: query_data.append(('max_id', str(max_id)))
+    # input query data
+    query_data = [('q', 'nba%20%28lebron%20OR%20kobe%29'), ('count', '100'), ('result_type', 'recent'), ('lang', 'en')]
+    if max_id != 0:
+        query_data.append(('max_id', str(max_id)))
     query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
     response = requests.get(query_url, auth=my_auth, stream=True)
     print(query_url, response)
@@ -24,36 +26,25 @@ def get_7days_tweets(max_id=0):
 
 
 def convert_tweets_to_csv(http_resp):
-    try:
-        with open('tweets_test_7days.csv', 'a+') as file:
-            tweet_lists = []
+    with open('tweets_test_7days.csv', 'a+') as file:
+        tweet_lists = []
+        for resp in http_resp.iter_lines():
+            statuses = json.loads(resp)['statuses']
+            last_id = statuses[len(statuses) - 1]['id']
+            for status in statuses:
+                line = []
+                # save into csv file
+                # tweet_id, create_date, content, favorite_count, retweet_count
+                line.append(str(status['id']))
+                line.append(str(status['created_at']))
+                line.append(str(status['text']))
+                line.append(str(status['favorite_count']))
+                line.append(str(status['retweet_count']))
+                df = pd.DataFrame(line).T
+                df.to_csv(file, header=['tweet_id', 'create_date', 'content', 'favorite_count', 'retweet_count'], index=False)
+                tweet_lists.append(line)
 
-            for resp in http_resp.iter_lines():
-                    statuses = json.loads(resp)['statuses']
-                    last_id = statuses[len(statuses) - 1]['id']
-                    for status in statuses:
-                        line = []
-                        # 保存相关属性到csv中
-                        # id, user_id, created_at, user_location, text
-                        # timeusername text source location
-                        line.append(str(status['id']))
-                        line.append(str(status['user']['id']))
-                        line.append(str(status['created_at']))
-                        # line.append(status.user.screen_name.encode('utf-8'))
-                        # line.append(status.source.encode('utf-8'))
-                        if status['user']['location'] is not None:
-                            line.append(status['user']['location'])
-                        else:
-                            line.append('None')
-                        line.append(status['text'].replace('\n', ' ').encode('utf-8'))
-                        df = pd.DataFrame(line).T
-                        df.to_csv(file, header=False, index=False)
-                        tweet_lists.append(line)
-        return last_id
-    except Exception as e:
-        print("resp", resp)
-        print("e", e)
-
+    return last_id
 
 
 print("Starting getting tweets.")
@@ -61,10 +52,8 @@ resp = get_7days_tweets()
 count = 1
 while True:
     count += 1
-    # if number of request is 180 (rate_limit for 15mins) then break
     if count == 180:
         break
     last_id = convert_tweets_to_csv(resp)
     resp = get_7days_tweets(last_id)
 print(count)
-# convert_tweets_to_csv(resp)
