50ae7588c6d9596c06e400e3046fca51efb96a00
“ynyhxfo” <junhuihe@usc.edu>
2019-11-22 02:57:50 -0800

    Jasper/upload_to_s3

--- new_twitter_search_api.py
+++ new_twitter_search_api.py
@@ -73,6 +73,26 @@ def get_query(query_list):
     return string
 
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 # start scraping function
 def run(num, query_list, ttID):
     # create csv file
@@ -94,3 +114,4 @@ def run(num, query_list, ttID):
             break
         last_id = convert_tweets_to_csv(response, ttID)
         response = get_7days_tweets(last_id)
+    upload_file('{}.csv'.format(ttID), 'demoteam10')
