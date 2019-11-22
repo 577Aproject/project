4d3d6c648f1ca9ef8288c43f8aca34a49609eb9f
leizhang323 <42682317+leizhang323@users.noreply.github.com>
2019-11-21 22:32:08 -0800

    Added file header and provided api to get parameters

--- new_twitter_search_api.py
+++ new_twitter_search_api.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import requests
 import requests_oauthlib
+import csv
 
 # Replace the values below with yours
 CONSUMER_KEY = '2lqt4zKdTSyueMSosEyADkZuq'
@@ -9,39 +10,38 @@ CONSUMER_SECRET = 'tQHyN0oR46GCGGn7uOTKTFdA5ivB8ixFUNYp4JB8o88rflNh6w'
 ACCESS_TOKEN = '1183084435421351937-wh2G26PTPmvfY5oM37w2CamNkVvcb1'
 ACCESS_SECRET = '33ljJ3n3Mm5MxRUYcQfEgN8Zi6WPErVvCZB7PctO0i24d'
 my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
-query_data = [('q', str(get_query(query_list))),
+
+# default query data
+query_data = [
             ('count', '100'),
             ('result_type', 'recent'),
             ('lang', 'en')
             ]
 
-
 # get request response
 def get_7days_tweets(max_id=0):
-    #    30days url
-    #     url = 'https://api.twitter.com/1.1/tweets/search/30day/tweets.json'
+    #    if we want to use 30days url
+    #    url = 'https://api.twitter.com/1.1/tweets/search/30day/tweets.json'
     url = 'https://api.twitter.com/1.1/search/tweets.json'
     # input query data
-
     if max_id != 0:
         query_data.append(('max_id', str(max_id)))
     query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
     response = requests.get(query_url, auth=my_auth, stream=True)
     print(query_url, response)
+    if max_id != 0:
+        query_data.remove(('max_id', str(max_id)))
     return response
 
 # save data into csv file
-def convert_tweets_to_csv(http_resp):
-    with open('tweets_test_7days.csv', 'a+') as file:
-        for resp in http_resp.iter_lines():
+def convert_tweets_to_csv(response):
+    with open('result.csv', 'a+') as file:
+        for resp in response.iter_lines():
             statuses = json.loads(resp)['statuses']
-            if len(statuses) > 1:
-                last_id = statuses[len(statuses) - 1]['id']
-            else:
-                break
+            last_id = statuses[len(statuses) - 1]['id']
             for status in statuses:
                 line = []
-                # save into csv file
+                # save response statuses data into csv file
                 # tweet_id, create_date, content, favorite_count, retweet_count
                 line.append(str(status['id']))
                 line.append(str(status['created_at']))
@@ -55,13 +55,17 @@ def convert_tweets_to_csv(http_resp):
 
 # get query based on parameters
 def get_query(query_list):
+
     string = ''
+
     # only has key words:
     if len(query_list) == 1:
         string = query_list[0]
+
     # have two key words:
     if len(query_list) == 2:
         string = query_list[0] + '%20' + query_list[1]
+
     # have more than two key words
     if len(query_list) > 2:
         string = query_list[0] + '%20%28'
@@ -70,29 +74,30 @@ def get_query(query_list):
                 string = string + query_list[i] + '%20OR%20'
             else:
                 string = string + query_list[i] + '%29'
-
     return string
 
 # start scraping function
 def run(num, query_list):
-    resp = get_7days_tweets()
-    count = 1
+
+    # create csv file
+    with open('result.csv', 'w', newline='') as f:
+        head = ['id', 'created_at', 'text', 'favorite_count', 'retweet_count']
+        writer = csv.writer(f)
+        writer.writerow(head)
+
+    # get search query
+    query_data.append(('q', str(get_query(query_list))))
+
+    # get response
+    response = get_7days_tweets()
+    # statuses = json.loads(response)['statuses']
+    # print(statuses)
+    count = 0
+
+    # get the other part of data
     while True:
         count += 1
-        if count == num:
+        if count == num + 1:
             break
-        last_id = convert_tweets_to_csv(resp)
-        resp = get_7days_tweets(last_id)
-    print(count)
-
-# if __name__ == '__main__':
-
-#     query_list = ['nba', 'kobe', 'curry']
-#     num = 10
-#     query_data = [('q', str(get_query())),
-#                   ('count', '100'),
-#                   ('result_type', 'recent'),
-#                   ('lang', 'en')
-#                   ]
-#     print("Starting getting tweets.")
-#     run(num)
+        last_id = convert_tweets_to_csv(response)
+        response = get_7days_tweets(last_id)
