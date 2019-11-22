f5d7e9cd649e5bc0a4df5dbdce798e47d1df5de6
“ynyhxfo” <junhuihe@usc.edu>
2019-11-21 19:57:50 -0800

    modify_filter_method

--- filter_imdb_data.py
+++ filter_imdb_data.py
@@ -1,17 +1,21 @@
+# useage: filter duplicate imdb movies. 
+# The first argument is the file to filter. The second file is the output file.
+
 import csv
 import sys
 
-csv.field_size_limit(sys.maxsize)
+if __name__ == "__main__":
 
-filter_file = open('/Users/jasper/Downloads/filter.tsv', 'w')
-tsv_writer = csv.writer(filter_file, delimiter='\t')
+    csv.field_size_limit(sys.maxsize)
+    filter_file = open(sys.argv[2], 'w')
+    tsv_writer = csv.writer(filter_file, delimiter='\t')
 
-with open('/Users/jasper/Downloads/movie_titles.tsv') as tsvfile:
-    reader = csv.reader(tsvfile, delimiter='\t')
-    tsv_writer.writerow(next(reader))
-    for row in reader:
-        if row[3] != 'US':
-            continue
-        tsv_writer.writerow(row)
+    with open(sys.argv[1]) as tsvfile:
+        reader = csv.reader(tsvfile, delimiter='\t')
+        tsv_writer.writerow(next(reader))
+        for row in reader:
+            if row[3] != 'US':
+                continue
+            tsv_writer.writerow(row)
 
-filter_file.close()
+    filter_file.close()
